9400e6caa24e27269ddd6c37b92ebd04701b5e9e
macksonjartinez <martinezjackson2001@gmail.com>
2021-03-30 22:03:30 -0400

    Cambios finales

---
+++ Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prog_3___Parcial_2.Shared
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Cedula_Querellante { get; set; }
+        public string Nombre_Querellante { get; set; }
+        public string Fecha { get; set; }
+        public string Objeto_Robado { get; set; }
+        public string Lugar { get; set; }
+        public string Valor_Objeto { get; set; }
+        public string Latitud { get; set; }
+        public string Longitud { get; set; }
+    }
+}
